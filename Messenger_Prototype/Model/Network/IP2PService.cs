e04c6b51489b69683f59bb61cdbf360b158c3b0f
Fedor <qwertysdagger@gmail.com>
2017-10-08 16:16:44 +0300

    First version.

---
+++ Messenger_Prototype/Model/Network/IP2PService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Messenger_Prototype
+{
+    [ServiceContract]
+    public interface IP2PService
+    {
+        [OperationContract]
+        string GetName();
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message, string from);
+    }
+}
---
+++ Messenger_Prototype/Model/Network/PeerEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.PeerToPeer;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger_Prototype
+{
+    public class PeerEntry
+    {
+        public PeerName PeerName { get; set; }
+        public IP2PService ServiceProxy { get; set; }
+        public string DisplayString { get; set; }
+        public bool ButtonsEnabled { get; set; }
+    }
+}
