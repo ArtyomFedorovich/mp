160f366ba8afca970e6f3f7bc09f19251144b401
Fedor <qwertysdagger@gmail.com>
2017-12-13 23:24:45 +0300

    Add some kind of structure.

---
+++ Chat/ModelNetwork/UdpPacketListener.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+  /// <summary>
+  /// Object that do listen to incoming UDP packets.
+  /// </summary>
+  public class UdpPacketListener
+  {
+    public void UdpListen()
+    {
+      UdpClient listener = new UdpClient();
+      IPEndPoint socket = new IPEndPoint(IPAddress.Any, 48911);
+      listener.Client.Bind(socket);
+
+      while (true)
+      {
+        try
+        {
+          byte[] pdata = listener.Receive(ref socket);
+          string message = Encoding.Unicode.GetString(pdata);
+          if (!string.IsNullOrEmpty(message))
+          {
+            UpdateView(message);
+          }
+          Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+        finally
+        {
+          //listener.Close();
+        }
+      }
+    }
+  }
+}
---
+++ Chat/Views/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Chat
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      Thread thread = new Thread(new UdpPacketListener().UdpListen);
+      thread.IsBackground = true;
+      InitializeComponent();
+      thread.Start();
+    }
+
+    public delegate void UpdateContent(string message);
+    private void UpdateTextBoxChatPane(string message)
+    {
+      textBoxChatPane.Text += message;
+    }
+    private void UpdateView(string message)
+    {
+      var op = textBoxChatPane.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+        new UpdateContent(UpdateTextBoxChatPane), message);
+    }
+
+    #region FormEvents
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void TextBoxEntryField_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Return || e.Key == Key.Enter)
+      {
+        UdpClient senderich = null;
+        try
+        {
+          senderich = new UdpClient();
+          IPEndPoint receiver = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 48911);
+          byte[] bytes = Encoding.Unicode.GetBytes(textBoxEntryField.Text);
+          senderich.Send(bytes, bytes.Length, receiver);
+        }
+        finally
+        {
+          senderich.Close();
+        }
+        textBoxEntryField.Clear();
+        //Thread.Sleep(TimeSpan.FromSeconds(5));
+      }
+    }
+    #endregion
+  }
+}
